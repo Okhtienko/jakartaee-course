63f3903c912b1907612876e04af3b31385c217c3
Okhtienko <mariohtienko@gmail.com>
2022-10-24 10:34:12 +0300

    added plugins

--- lesson24/src/main/java/com/technology/repository/JdbcUserRepository.java
+++ lesson24/src/main/java/com/technology/repository/JdbcUserRepository.java
@@ -1,14 +1,13 @@
 package com.technology.repository;
 
-import java.util.Optional;
-import java.util.List;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import com.technology.model.User;
 
---
+++ lesson24/src/test/java/com/technology/service/UserServiceTest.java
@@ -0,0 +1,58 @@
+package com.technology.service;
+
+import java.util.Optional;
+
+import com.technology.model.User;
+import com.technology.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.mock;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+  @Mock
+  private UserRepository userRepository;
+
+  @InjectMocks
+  private UserService sut;
+
+  @Test
+  void shouldCreateUserWhenUserNotExist() {
+    final String name = "any_name";
+    final String password = "any_password";
+    final String role = "any_role";
+
+    given(userRepository.getUser(name)).willReturn(Optional.empty());
+
+    sut.addUser(name, password);
+
+    then(userRepository)
+        .should()
+        .addUser(name, password);
+  }
+
+  @Test
+  void shouldThrowExceptionWHenUserExists() {
+    final String name = "any_name";
+    final String password = "any_password";
+    final String role = "any_role";
+    final User user = mock(User.class);
+
+    given(userRepository.getUser(name)).willReturn(Optional.of(user));
+
+    final RuntimeException actual = assertThrows(
+        RuntimeException.class, () -> sut.addUser(name, password));
+
+    assertThat(actual)
+        .hasMessage("User already exists");
+
+  }
+}
